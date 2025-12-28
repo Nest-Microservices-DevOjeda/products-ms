aab302ba9b598129350c2cf46c0af12182e54435
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-12-28 12:27:33 -0600

    feat: implement pagination DTO and integrate with products controller and service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -15,7 +15,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prismaService.product.findMany({});
   }
 
   findOne(id: number) {
